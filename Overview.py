f935e0e722dda21401041c8c6655b7ede62ff43b
Abuh <ak.abrahamkibet@gmail.com>
2024-02-23 21:05:11 +0300

    First Comit

---
+++ Overview.py
@@ -0,0 +1,57 @@
+#*** Comments ***
+"""print("Hello Abuh")""" # This is also a comment for long paragraphs of code
+
+
+# VARIABLES - Conatiner to hold data
+number = 10
+site_name = 'Power learn Project' # Declaring variable
+print(number)
+print(site_name)
+
+site_name = 'apple.com' # changing value of variables
+print(site_name)
+
+a, b, c = 5, 3.2, 'Abuh' # Assigning multiple values to variables
+print(a)
+print(b)
+print(c)
+
+site1 = site2 = 'webmasters'
+print(site1)
+print(site2)
+
+#DATATYPES
+num1 = 5 
+num2 = 8.9
+num3 = 98+9
+print(num1, 'is a type of', type(num1))
+print(num2, 'is a type of', type(num2))
+print(num3, 'is a type of', type(num3))
+
+#1.LIST
+programming_languages = ["java", 'js', "python"]
+print(programming_languages)
+print(programming_languages[0])
+print(programming_languages[2])
+print(programming_languages[1])
+
+# 2.TUPLES - IMMUTABLE
+product = ('xbox', 'playstation', 353)
+print(product)
+print(product[0])
+print(product[1])
+
+#3. STRING
+message = 'First python code'
+print(message)
+
+# 4.  SET
+student_id = {112, 114, 115}
+print(student_id)
+
+#5. DICTIONARY
+capital_city = {'Nepal': 'kathmandu', 'italy': 'rome', 'kenya': 'nairobi'} # nepal,italy and Kenya are the keys
+print(capital_city)
+print(capital_city['Nepal'])
+print(capital_city['kenya'])
+print(capital_city['italy'])
