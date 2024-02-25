5fe56cdb01ac0914d974f651cc06de6e102364fb
Abuh <ak.abrahamkibet@gmail.com>
2024-02-25 22:08:59 +0300

    first commit

--- Overview.py
+++ Overview.py
@@ -55,3 +55,62 @@ print(capital_city)
 print(capital_city['Nepal'])
 print(capital_city['kenya'])
 print(capital_city['italy'])
+
+# TYPE CONVERTION
+#1. IMPLICIT CONVERTION - Automatic type convertion
+integer_number = 123
+float_number = 1.23
+new_number = integer_number + float_number
+print(new_number)
+print(type(new_number))
+
+
+# 2. EXPLICIT TYPE CONVERTION - Manual convertion
+num_string = '12'
+num_integer = 23
+
+print(type(num_string))
+
+num_string = int(num_string)
+print(type(num_string))
+
+
+#TYPE OF PYTHON OPERATORS
+# 1. ARITHMETIC OPERATOR
+ """ 
+   + to add a and b
+    - to subtract b from a
+    * to multiply a and b
+    / to divide a by b
+    // to floor divide a by b
+    % to get the remainder
+    ** to get a to the power b"""
+
+# 2. ASSIGNMENT OPERATOR
+"""
+a = 7 #Assignment
+a += 1 # Addition
+a -+ 3 #Subtraction
+a *= 5 #Multiplication
+a /= 6 #Division
+a %= 8 # Remainder
+a **= 10 # Exponent
+"""
+
+# 3. COMPARISON OPERATOR
+
+"""
+3 == 5 
+3 != 5
+3 > 5
+3 < 5
+3 >= 5
+3 <= 5
+"""
+
+#LOGICAL OPERATORS
+"""
+and 
+or
+not
+"""
